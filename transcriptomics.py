4b7f69a737e5832b8f6ee37b2670337ba6735dc1
ciandotson <94946933+ciandotson@users.noreply.github.com>
2025-02-24 03:20:54 -0600

    Update transcriptomics.py

--- transcriptomics.py
+++ transcriptomics.py
@@ -141,8 +141,8 @@ os.system('echo "$(cat hcmv_sigs.tsv)" >> PipelineProject.log') # write the outp
 
 os.system('bowtie2-build "hcmv_genome.fasta" ./hcmv_gen/hcmv_gen') # create a bowtie index from teh full genome of the virus #
 
-os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660030_1.fastq -2 ./fastqs/SRR5660030_2.fastq -S d1i2.sam' ) # map the reads from the transcriptome to the refernce genome of the virus in sample 1 #
-os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660033_1.fastq -2 ./fastqs/SRR5660033_2.fastq -S d1i6.sam') # map the reads from the transcriptome to the refernce genome of the virus in sample 2 #
-os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660044_1.fastq -2 ./fastqs/SRR5660044_2.fastq -S d3i2.sam') # map the reads from the transcriptome to the refernce genome of the virus in sample 3 #
-os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660045_1.fastq -2 ./fastqs/SRR5660045_2.fastq -S d3i6.sam') # map the reads from the transcriptome to the refernce genome of the virus in sample 4 #
+os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660030_1.fastq -2 ./fastqs/SRR5660030_2.fastq -S d1i2.sam --al-conc d1i2_mapped_reads.fq --un-conc d1i2_unmapped_reads.fq') # map the reads from the transcriptome to the refernce genome of the virus in sample 1 #
+os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660033_1.fastq -2 ./fastqs/SRR5660033_2.fastq -S d1i6.sam --al-conc d1i6_mapped_reads.fq --un-conc d1i6_unmapped_reads.fq') # map the reads from the transcriptome to the refernce genome of the virus in sample 2 #
+os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660044_1.fastq -2 ./fastqs/SRR5660044_2.fastq -S d3i2.sam --al-conc d3i2_mapped_reads.fq --un-conc d3i2_unmapped_reads.fq') # map the reads from the transcriptome to the refernce genome of the virus in sample 3 #
+os.system('bowtie2 --quiet -x ./hcmv_gen/hcmv_gen -1 ./fastqs/SRR5660045_1.fastq -2 ./fastqs/SRR5660045_2.fastq -S d3i6.sam --al-conc d3i6_mapped_reads.fq --un-conc d3i6_unmapped_reads.fq') # map the reads from the transcriptome to the refernce genome of the virus in sample 4 #
 
