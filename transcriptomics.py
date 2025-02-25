21e1cc5594cabf62253d49965ff6507b69f01577
ciandotson <94946933+ciandotson@users.noreply.github.com>
2025-02-24 21:33:45 -0600

    Update transcriptomics.py

--- transcriptomics.py
+++ transcriptomics.py
@@ -18,7 +18,7 @@ args = check_arg(sys.argv[1:])
 infile = args.input
 outfile = args.output
 
-var=str(infile)
+os.system('var="demo_fastq"')
 # formating the CDS entries of HCMV into a fasta that only contains the protein IDs as entries #
 from Bio import SeqIO
 import re
